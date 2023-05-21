cd09e5f19824e7a5a4ba389a217409e140085d1d
pc <hoangja12@gmail.com>
2023-05-21 14:05:52 +0700

    21/5

--- app/src/main/java/com/vndevpro/android52_day3/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.vndevpro.android52_day3;
-
-public interface Constants {
-
-    String USER= "USER";
-    String USER_NAME = "USER_NAME";
-    String PASSWORDS = "PASSWORDS";
-    String PHONE = "PHONE";
-}
--- app/src/main/java/com/vndevpro/android52_day3/MainActivity.java
+++ app/src/main/java/com/vndevpro/android52_day3/MainActivity.java
@@ -1,70 +1,40 @@
 package com.vndevpro.android52_day3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Fragment";
 
-    private EditText edtName;
-    private EditText edtEmail;
-    private EditText edtPassword;
-    private EditText edtPhoneNunber;
-   private TextView tvLogin;
-
-   private Button btnJohnUs;
-
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_register);
-
+        setContentView(R.layout.activity_main);
         initView();
+
     }
 
     private void initView() {
-        btnJohnUs = findViewById(R.id.btnJoinUs);
-        edtName = findViewById(R.id.edtName);
-        edtPassword = findViewById(R.id.edtPassword);
-        edtEmail = findViewById(R.id.edtEmail);
-        edtPhoneNunber = findViewById(R.id.edtPhoneNumber);
+        ButterKnife.bind(this);
 
-        btnJohnUs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                Intent intent = new Intent();
-
-                String userName = edtName.getText().toString();
-                String passwords = edtPassword.getText().toString();
-                String phone =edtPhoneNunber.getText().toString();
-                String email = edtEmail.getText().toString();
-
-                UserModel userModel = new UserModel();
-                userModel.setName(userName);
-                userModel.setPassword(passwords);
-                userModel.setPhone(phone);
-                userModel.setEmail(email);
-                intent.putExtra(Constants.USER, userModel);
-                setResult(RESULT_OK, intent);
-                finish();
-            }
-        });
+        boolean isLogin = getSharedPreferences("LOGIN", MODE_PRIVATE).getBoolean("LOGIN_STATE", false);
+        if (isLogin) {
+            HomeFragment homeFragment = new HomeFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, homeFragment).addToBackStack("ADD").commit();
+        } else {
+            loginFragment = new LoginFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, loginFragment).addToBackStack("ADD").commit();
+        }
     }
+
+    LoginFragment loginFragment;
+
 }
